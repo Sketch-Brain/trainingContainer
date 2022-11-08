62f7876fea833cf0dbea2a1a8269b48701f95c69
TrulyNotMalware <notleebutyee@gmail.com>
2022-11-08 17:59:07 +0900

    TOKEN Value changed default -> to Environ Value

--- app/dependencies.py
+++ app/dependencies.py
@@ -5,12 +5,10 @@ from fastapi import Header, HTTPException
 
 
 async def get_token_header(x_token: str = Header(...)):
-    # if x_token != os.environ.get("X_TOKEN"):
-    if x_token != "run":
+    if x_token != os.environ.get("X_TOKEN"):
         raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
 async def get_query_token(token: str):
-    # if token != os.environ.get("TOKEN"):
-    if token != "sketch":
+    if token != os.environ.get("TOKEN"):
         raise HTTPException(status_code=400, detail="No Jessica token provided")
